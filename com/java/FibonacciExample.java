02b7af1b584e64b8cfbbf16815db48053539b4a1
“Harishkumar” <mhkharish192003@gmail.com>
2024-06-17 12:39:44 +0530

    fibonacii

--- com/java/FibonacciExample.java
+++ com/java/FibonacciExample.java
@@ -4,7 +4,9 @@ class FibonacciExample{
 public static void main(String[] args)  
 {    
  int n1=0;
- int n2=1,n3,n5;
+ int n2=1;
+ int n3;
+ int var=0;
  int i;
  int count=10;    
  System.out.print(n1+" "+n2);   
